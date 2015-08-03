a8308f8a1fc0432bb3e855b6b0d2deb7b57b575c
Stewart Charles <trio.of.four@gmail.com>
2015-08-03 08:44:16 -0600

    Added additional Rustdoc comments.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod game {
     //! 'server' module will issue the creation, and control of the various
     //! game components.
     
+    /* Static Array's corresponding to the ordinal values and suites of a card */
     static SUITES:[&'static str;4] = ["Heart", "Spade", "Diamond", "Club"];
     static VALUES:[&'static str;13] = ["Ace","Two", "Three", "Four", "Five",
                                        "Six", "Seven", "Eight", "Nine","Ten",
@@ -35,7 +36,8 @@ mod game {
             (VALUES[self.value], SUITES[self.suite])
         }
     }
-    
+
+    /// A Container for Cards including a draw pile and a discard pile.
     pub struct Deck {
         card_deck:Vec<Card>,
         discard_pile:Vec<Card>,
@@ -43,6 +45,8 @@ mod game {
     }
     
     impl Deck {
+
+        ///Creates a new, unshuffled deck containing the standard 52 cards.
         pub fn new()->Deck {
             let mut cards = Vec::new();
             let discard = Vec::new();
@@ -55,6 +59,8 @@ mod game {
             Deck{card_deck:cards, discard_pile:discard, current:0}
         }
 
+        /// Merges the Decks discard pile with the draw pile and shuffles
+        /// the draw pile in place. 
         pub fn shuffle(&mut self) {
             let mut rng = thread_rng();
             while self.discard_pile.len() > 0 {
@@ -67,6 +73,17 @@ mod game {
 
     impl Iterator for Deck {
         type Item = Card;
+
+        /// impl of Iterator::next function allows use of foreach style
+        /// iterator on a deck type directly.
+        /// # Examples
+        /// '''
+        /// let deck = Deck::new();
+        /// for card in deck {
+        ///     let c_tup = card.to_string_pair();
+        ///     println!("{_val} of {_suite}s", _val=c_tup.0, _val=c_tup.1);
+        /// }
+        /// '''
         fn next(&mut self)->Option<Card> {
             let length = self.card_deck.len();
             if self.current < length {
@@ -83,6 +100,13 @@ mod game {
 
     impl Index<usize> for Deck {
         type Output = Card;
+
+        /// impl of Index::index allows indexed access to deck
+        /// # Examples
+        /// '''
+        /// let deck = Deck::new();
+        /// let card = deck[1];
+        /// '''
         fn index<'a>(&'a self, _index:usize)->&'a Card {
             &self.card_deck[_index]
         }
